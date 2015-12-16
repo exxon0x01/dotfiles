a7de438fe16e3a01ef48cd38c450b0f592f073ab
exxon0x01 <exxon0x@gmail.com>
2015-12-16 12:14:34 +0900

    add packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,6 +7,8 @@
 (set-default-coding-systems 'utf-8)
 (prefer-coding-system 'utf-8)
 
+(load-theme 'wheatgrass t)
+
 (setq inhibit-startup-message t)
 
 (setq make-backup-files nil)
@@ -16,5 +18,50 @@
 (setq-default tab-width 4 indent-tabs-mode nil)
 
 (global-linum-mode t)
+(setq linum-format "%4d ")
 
 (fset 'yes-or-no-p 'y-or-n-p)
+
+;;keybind
+(global-set-key "\C-h" 'backward-char)
+(global-set-key "\C-j" 'next-line)
+(global-set-key "\C-k" 'previous-line)
+(global-set-key "\C-l" 'forward-char)
+(global-set-key "\C-n" 'newline-and-indent)
+(global-set-key "\C-o" 'kill-line)
+
+;;package
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+(defvar my/favorite-packages
+    '(
+      ;;auto-complete
+      auto-complete popup auto-complete-c-headers
+      ))
+(dolist (package my/favorite-packages)
+    (unless (package-installed-p package)
+          (package-install package)))
+
+;;auto-complete
+(require 'auto-complete-config)
+(ac-config-default)
+(ac-set-trigger-key "TAB")
+
+;;cc-mode
+(require 'cc-mode)
+(add-hook 'c-mode-common-hook
+          (lambda()
+            (setq c-default-style "k&r")
+            (setq indent-tabs-mode nil)
+            (setq c-basic-offset 2)
+            ))
+(add-to-list 'auto-mode-alist '("\\.h\\' " . c++-mode))
+
+(defun my:ac-c-headers-init ()
+  (require 'auto-complete-c-headers)
+  (add-to-list 'ac-sources 'ac-source-c-headers))
+
+(add-hook 'c++-mode-hook 'my:ac-c-headers-init)
+(add-hook 'c-mode-hook 'my:ac-c-headers-init)
