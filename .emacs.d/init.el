386bdae70b558c8d0fc0577440fc87de1cd49089
exxon0x01 <exxon0x01@gmail.com>
2015-12-18 21:21:07 +0900

    add setting 4 cyg

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -73,6 +73,20 @@
 
 ;;slime
 (setq inferior-lisp-program "sbcl")
+
+;;setting for cygwin
+(when (eq system-type 'cygwin)
+  (defun slime-to-lisp-translation (filename)
+    (replace-regexp-in-string
+       "\n" "" (shell-command-to-string
+              (format "cygpath.exe --windows %s" filename))))
+  (defun lisp-to-slime-translation (filename)
+    (replace-regexp-in-string
+     "\n" "" (shell-command-to-string
+              (format "cygpath.exe --unix %s filename"))))
+  (setq slime-to-lisp-filename-function 'slime-to-lisp-translation)
+  (setq lisp-to-slime-filename-function 'lisp-to-slime-translation))
+
 (setq slime-net-coding-system 'utf-8-unix)
 (require 'slime)
 (slime-setup '(slime-repl slime-fancy slime-banner))
