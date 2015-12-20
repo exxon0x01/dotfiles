b75f1a8554026fd7c98b48aa0bf196934b58539a
exxon0x01 <exxon0x@gmail.com>
2015-12-20 21:37:18 +0900

    add keybind 4 window & hiwin

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -28,8 +28,56 @@
 (global-set-key "\C-l" 'forward-char)
 (global-set-key "\C-n" 'newline-and-indent)
 (global-set-key "\C-o" 'kill-line)
-
-(require 'generic-x)
+(define-prefix-command 'windmove-map)
+(global-set-key (kbd "C-q") 'windmove-map)
+(define-key windmove-map "h" 'windmove-left)
+(define-key windmove-map "j" 'windmove-down)
+(define-key windmove-map "k" 'windmove-up)
+(define-key windmove-map "l" 'windmove-right)
+(defun split-window-conditional ()
+  (interactive)
+  (if (> (* (window-height) 2) (window-width))
+       (split-window-vertically)
+       (split-window-horizontally)))
+  (define-key windmove-map "s" 'split-window-conditional)
+  
+(global-set-key "\C-q\C-q" 'quoted-insert)
+;;windowsize
+(defun window-resizer ()
+  "Control window size and position."
+  (interactive)
+  (let ((window-obj (selected-window))
+        (current-width (window-width))
+        (current-height (window-height))
+        (dx (if (= (nth 0 (window-edges)) 0) 1
+              -1))
+        (dy (if (= (nth 1 (window-edges)) 0) 1
+              -1))
+        action c)
+    (catch 'end-flag
+      (while t
+        (setq action
+              (read-key-sequence-vector (format "size[%dx%d]"
+                                                (window-width)
+                                                (window-height))))
+        (setq c (aref action 0))
+        (cond ((= c ?l)
+               (enlarge-window-horizontally dx))
+              ((= c ?h)
+               (shrink-window-horizontally dx))
+              ((= c ?j)
+               (enlarge-window dy))
+              ((= c ?k)
+               (shrink-window dy))
+              ;; otherwise
+              (t
+               (let ((last-command-char (aref action 0))
+                     (command (key-binding action)))
+                 (when command
+                   (call-interactively command)))
+               (message "Quit")
+                              (throw 'end-flag t)))))))
+(global-set-key "\C-c\C-a" 'window-resizer)
 
 ;;package
 (require 'package)
@@ -51,7 +99,7 @@
     ;;color
     rainbow-delimiters
     ;;window
-    neotree
+    neotree hiwin
     ))
 (let ((not-installed (loop for x in installing-package-list
                            when (not (package-installed-p x))
@@ -60,6 +108,9 @@
     (package-refresh-contents)
     (dolist (pkg not-installed)
               (package-install pkg))))
+
+;;generic-x
+(require 'generic-x)
 
 ;;auto-complete
 (require 'auto-complete-config)
@@ -170,3 +221,8 @@
             (lambda () (setq neo-persist-show nil)))
   (add-hook 'popwin:after-popup-hook
             (lambda () (setq neo-persist-show t))))
+
+;;hiwin
+(require 'hiwin)
+(hiwin-activate)
+(set-face-background 'hiwin-face "gray10")
