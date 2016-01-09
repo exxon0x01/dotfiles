8d276af0d9f0bae5a70fe18487fdb9eae3359097
exxon0x01 <exxon0x@gmail.com>
2016-01-10 01:31:38 +0900

    divide clojure settings

---
+++ .emacs.d/clojure-setting.el
@@ -0,0 +1,15 @@
+(require 'cider)
+(require 'clojure-mode)
+(add-hook 'clojure-mode-hook 'cider-mode)
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(setq nrepl-hide-special-buffers t)
+(setq nrepl-buffer-name-show-port t)
+
+(require 'ac-cider)
+(add-hook 'cider-mode-hook 'ac-flyspell-workaround)
+(add-hook 'cider-mode-hook 'ac-cider-setup)
+(add-hook 'cider-repl-mode-hook 'ac-cider-setup)
+(eval-after-load "auto-complete"
+  '(progn
+     (add-to-list 'ac-modes 'cider-mode)
+          (add-to-list 'ac-modes 'cider-repl-mode)))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -148,22 +148,8 @@
 ;; Connections
 (push '(slime-connection-list-mode) popwin:special-display-config)
 
-;;cider
-(require 'cider)
-(require 'clojure-mode)
-(add-hook 'clojure-mode-hook 'cider-mode)
-(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-(setq nrepl-hide-special-buffers t)
-(setq nrepl-buffer-name-show-port t)
-
-(require 'ac-cider)
-(add-hook 'cider-mode-hook 'ac-flyspell-workaround)
-(add-hook 'cider-mode-hook 'ac-cider-setup)
-(add-hook 'cider-repl-mode-hook 'ac-cider-setup)
-(eval-after-load "auto-complete"
-  '(progn
-     (add-to-list 'ac-modes 'cider-mode)
-          (add-to-list 'ac-modes 'cider-repl-mode)))
+;;clojure
+(load "clojure-setting")
 
 ;;rainbow-delimiters
 (require 'rainbow-delimiters)
