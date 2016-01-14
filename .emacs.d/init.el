093f33124b9b36937bff55014e0bc420a4bb39a3
exxon0x01 <exxon0x@gmail.com>
2016-01-14 22:59:27 +0900

    divide cl-settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -104,31 +104,6 @@
 (add-hook 'c++-mode-hook 'my:ac-c-headers-init)
 (add-hook 'c-mode-hook 'my:ac-c-headers-init)
 
-;;slime
-(setq inferior-lisp-program "sbcl")
-
-;;slime-setting for cygwin
-(when (eq system-type 'cygwin)
-  (defun slime-to-lisp-translation (filename)
-    (replace-regexp-in-string
-     "\n" "" (shell-command-to-string
-              (format "cygpath.exe --windows %s" filename))))
-  (defun lisp-to-slime-translation (filename)
-    (replace-regexp-in-string
-     "\n" "" (shell-command-to-string
-              (format "cygpath.exe --unix %s filename"))))
-  (setq slime-to-lisp-filename-function 'slime-to-lisp-translation)
-  (setq lisp-to-slime-filename-function 'lisp-to-slime-translation))
-;;
-
-(setq slime-net-coding-system 'utf-8-unix)
-(require 'slime)
-(slime-setup '(slime-repl slime-fancy slime-banner))
-
-(require 'ac-slime)
-(add-hook 'slime-mode-hook 'set-up-slime-ac)
-(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
-
 ;;popwin
 (require 'popwin)
 (setq display-buffer-function 'popwin:display-buffer)
@@ -147,6 +122,9 @@
 (push '(slime-repl-mode) popwin:special-display-config)
 ;; Connections
 (push '(slime-connection-list-mode) popwin:special-display-config)
+
+;;common lisp
+(load "cl-setting")
 
 ;;clojure
 (load "clojure-setting")
